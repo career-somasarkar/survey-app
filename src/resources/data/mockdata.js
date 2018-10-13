358522707f0d926e1e4b288c470a629295a1fbee
Soma Sarkar <career.somasarkar@gmail.com>
2018-10-13 13:39:30 +0200

    added resources

---
+++ src/resources/data/mockdata.js
@@ -0,0 +1,109 @@
+
+export const SURVEY_DATA = [
+    {
+      id: 1,
+      text: "What is your favourit color?",
+      optionType: "RADIO",
+      options: [
+        {
+          id: 1,
+          text: "Red"
+        },
+        {
+          id: 2,
+          text: "Green"
+        },
+        {
+          id: 3,
+          text: "Blue"
+        },
+        {
+          id: 4,
+          text: "Orange"
+        }
+      ],
+      answer: {
+        selectedOption: {
+          id: 2
+        },
+        value: null
+      }
+    },
+    {
+      id: 2,
+      text: "Describe your hobbies:",
+      optionType: "OPEN_TEXT",
+      options: [
+        
+      ],
+      answer: {
+        selectedOption: null,
+        value: "I cant say"
+      }
+    },
+    {
+      id: 3,
+      text: "Q3",
+      optionType: "OPEN_TEXT",
+      options: [],
+      answer: {
+        selectedOption: null,
+        value: "Some text"
+      }
+    },
+    {
+      id: 4,
+      text: "Q4",
+      optionType: "RADIO",
+      options: [
+        {
+          id: 1,
+          text: "a"
+        },
+        {
+          id: 2,
+          text: "b"
+        },
+        {
+          id: 3,
+          text: "c"
+        }
+      ],
+      answer: {
+        selectedOption: {
+          id: 3
+        },
+        value: null
+      }
+    },{
+      id: 5,
+      text: "Q5",
+      optionType: "RADIO",
+      options: [
+        {
+          id: 1,
+          text: "a"
+        },
+        {
+          id: 2,
+          text: "b"
+        },
+        {
+          id: 3,
+          text: "a"
+        },
+        {
+          id: 4,
+          text: "b"
+        }
+      ],
+      answer: {
+        selectedOption: {
+          id: 4
+        },
+        value: null
+      }
+    }
+  ]
+ ; 
+  
